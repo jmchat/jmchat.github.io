1548ca97b48454256e498285b1e8c9224877220c
jmchat <46917781+jmchat@users.noreply.github.com>
2020-05-12 15:11:00 +0200

    Update service-worker.js

--- webapp/HTML/service-worker.js
+++ webapp/HTML/service-worker.js
@@ -4,7 +4,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 // Cache name
-var CACHE_NAME = 'cache-version-6';
+var CACHE_NAME = 'cache-version-9';
 
 // Files required to make this app work offline
 var REQUIRED_FILES = [
